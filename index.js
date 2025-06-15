c72162100dce412fa62b655876cb5039052a98c2
Asad <hellohello51981@gmail.com>
2025-06-15 20:32:38 +0500

    ADD JS - Update the inner HTml

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ formCollector.addEventListener('submit', event => {
     console.log("event");
     let ourFormdata = new FormData(event.target);
     let userFirstName = ourFormdata.get("firstName");
+    let updatedHtmlContent = `
+    <h2>Congratulation ${userFirstName}</h2>
+        <p>You're on your way to become a BBQ master.</p>
+        <p class="fine-print">We'll never share your information without your permission</p>
+        `
+        let mainContent = document.getElementById("main-content");
+        mainContent.innerHTML = updatedHtmlContent;
 
     })
